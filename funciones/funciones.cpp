28984e1ce1ca066f14bf4817a7bf89aa31ad2b18
Ricardo00899 <00000725@uca.edu.sv>
2025-06-03 12:33:47 -0600

    Ejercicios y clases

--- funciones/funciones.cpp
+++ funciones/funciones.cpp
@@ -38,7 +38,7 @@ float division (float uno, float dos){
 
 int main(){ //declaramos el cuerpo, las variabnles y desarrrollamios en el main
     float numero1, numero2, numero3=7, numero4=56, sumatoria=0, sumatoria2=0, resultadoResta=0, resultadoMultiplicacion=0, resultadoDivision=0;
-
+    int opcion;
     cout<<"Operaciones"<<endl;
     cout<<"Ingrese el primer numero"<<endl;
     cin>>numero1;
@@ -54,18 +54,38 @@ int main(){ //declaramos el cuerpo, las variabnles y desarrrollamios en el main
     resultadoMultiplicacion = multiplicacion(numero1,numero2);
     resultadoDivision = division(numero1,numero2);
 
-    cout<<"Resultado de la suma: "<<sumatoria<<endl;
-    cout<<"Resultado de la segunda suma: "<<sumatoria2<<endl;
-    cout<<"Resultado de la resta: "<<resultadoResta<<endl;
-    cout<<"Resultado de la multiplicacion: "<<resultadoMultiplicacion<<endl;
-    //planteamos una condicion en el caso que el usuario plantee una division que no se pueda realizar.
-    if(resultadoDivision=5000){
-        cout<<"La division no se puede"<<endl;
-    }
-    else{
-    cout<<"Resultado de la division: "<<resultadoDivision<<endl;
-    }
+    //planteamos un menu y un swich para el usuario pueda elegir la operacion que desea realizar
+    cout<< "Que operacion desea realizar?"<<endl;
+    cout<<"1. Suma"<<endl;
+    cout<<"2. Resta"<<endl;
+    cout<<"3. Multiplicacion"<<endl;
+    cout<<"4. Division"<<endl;
+    cout << "Digite la opcion: "<<endl;
+    cin >>opcion;
 
+    switch(opcion){
+        case 1:
+            cout<<"La suma es: "<<sumatoria<<endl;
+            break;
+        case 2:
+            cout<<"La resta es: "<<resultadoResta<<endl;
+            break;
+        case 3:
+            cout<<"La multiplicacion es: "<<resultadoMultiplicacion<<endl;
+            break;
+        case 4:
+        //planteamos una condicion en el caso que el usuario plantee una division que no se pueda realizar.
+        if(resultadoDivision=5000){
+        cout<<"La division no se puede"<<endl;
+        }else{
+        cout<<"Resultado de la division: "<<resultadoDivision<<endl;
+        }
+            break;
+        default:
+            cout<<"Opcion no valida"<<endl;
+    }
+    //planteamos una condicion en el caso que el usuario plantee una division que no se pueda realizar.
+    
 
     return 0;
 }
