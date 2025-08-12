d03f54d4bfa8a98b0bc09cd389628cdad452a1b1
Ricardo00899 <00000725@uca.edu.sv>
2025-08-12 12:39:32 -0600

    primer-snipet-clase-0

---
+++ PED/Semana 01/Clase_0.cc
@@ -0,0 +1,10 @@
+/*A partir de ahora se trabajara con archivos con extencion tipo "cc" y sin el uso de "using namespace std"*/
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Mi primer snippet";
+
+    return 0;
+}
